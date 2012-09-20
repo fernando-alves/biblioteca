09c22fb49e7e195facd1c027b4eaef491782841d
Fernando Alves da Silva Freitas Júnior <fernandoasfj@gmail.com>
2012-09-20 08:24:04 -0300

    refactoring

--- src/com/twu28/biblioteca/main/Main.java
+++ src/com/twu28/biblioteca/main/Main.java
@@ -2,9 +2,11 @@ package com.twu28.biblioteca.main;
 
 import com.twu28.biblioteca.library.Library;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
         Library.build().run();
     }
 }
